e6d58dd869b8edd4a30b810a09c42996ab7d47cf
EdgarAbyss <40816003+EdgarAbyss@users.noreply.github.com>
2022-11-30 10:07:39 -0600

    Traducir al español Backlinks
    Creo que está bien

--- components/MDContent.js
+++ components/MDContent.js
@@ -6,7 +6,7 @@ import {useRouter} from 'next/router'
 function BackLinks({linkList}) {
 
     return (<div className="note-footer">
-            <h3 className="backlink-heading">Link to this note</h3>
+            <h3 className="backlink-heading">Enlaces a esta nota</h3>
         {(linkList != null && linkList.length > 0)
             ?
             <>
@@ -24,7 +24,7 @@ function BackLinks({linkList}) {
                     )}
                 </div>
             </>
-            : <> <p className="no-backlinks"> No backlinks found</p> </>}
+            : <> <p className="no-backlinks"> No se encontraron enlaces</p> </>}
     </div>);
 }
 
